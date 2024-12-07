3c69f262ce23a78dfe6e6d9909d110101d9678ad
BugHunter-X <askzenai@gmail.com>
2024-12-08 00:12:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,8 @@
+function foo(a, b) {
+  if (a === null || b === null) {
+    // Handle null values appropriately. For example:
+    console.log('Null value encountered.');
+    return null; // or some default value
+  }
+  // ... rest of the function
+}
